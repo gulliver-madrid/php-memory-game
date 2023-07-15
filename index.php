87604d44424a6c542c4b00da679ce5d304701daf
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-07-15 12:31:02 +0200

    Use array $juego

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+
     require_once "helpers.php";
 
     $debug = false;
+
 
     // Iniciar el juego
     if (!isset($_SESSION)) {
@@ -9,60 +11,72 @@
     }
 
     // Verificar si se debe reiniciar el juego
-    if (isset($_GET['restart']) || !isset($_SESSION['cartas'])) {
+    if (isset($_GET['restart']) || !isset($_SESSION['juego'])){
+        $juego = array();
+        $_SESSION['juego'] = $juego;
         // Cartas del juego
         $cartas = array('A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H');
         // Barajar las cartas
         shuffle($cartas);
-
-        $_SESSION['cartas'] = $cartas;
-        $_SESSION['intento_actual'] = array();
-        $_SESSION['encontradas'] = array();
-        $_SESSION['intentos'] = 0;
-        $_SESSION['aciertos'] = 0;
+        $juego['cartas'] = $cartas;
+        $juego['intento_actual'] = array();
+        $juego['encontradas'] = array();
+        $juego['intentos'] = 0;
+        $juego['aciertos'] = 0;
+    } else {
+        $juego = $_SESSION['juego'];
     }
+
 
     if ($debug){
-        echo "<p>Cartas barajadas: ".arrayToString($_SESSION['cartas']).'</p>';
+        echo "<p>Cartas barajadas: ".arrayToString($juego['cartas']).'</p>';
+        echo "<p>intento_actual: ". arrayToString($juego['intento_actual'])."</p>";
+        echo "<p>encontradas: ". arrayToString($juego['encontradas'])."</p>";
+        echo "<p>juego: ". arrayToString($juego)."</p>";
     }
 
-    function registrarCarta(array $cartas)
+    function registrarCarta(array $juego)
     {
-        if (count($_SESSION['intento_actual']) >= 2){
+        if (count($juego['intento_actual']) >= 2){
             // Intento ya completado
-            return;
+            return $juego;
         }
         $carta = $_GET['carta'];
-        if (in_array($carta, $_SESSION['intento_actual'])) {
+        if (in_array($carta, $juego['intento_actual'])) {
             // Ya elegida
-            return;
+            return $juego;
         }
         // Anadimos la carta pulsada
-        $_SESSION['intento_actual'][] = $carta;
-        if (count($_SESSION['intento_actual']) != 2){
+        $juego['intento_actual'][] = $carta;
+        if (count($juego['intento_actual']) != 2){
             // Turno no completado
-            return;
+            return $juego;
         }
         // Evaluacion turno
-        $_SESSION['intentos']++;
-        $esAcierto = ($cartas[$_SESSION['intento_actual'][0]] == $cartas[$_SESSION['intento_actual'][1]]);
+        $juego['intentos']++;
+        $cartas = $juego['cartas'];
+        $esAcierto = ($cartas[$juego['intento_actual'][0]] == $cartas[$juego['intento_actual'][1]]);
         if ($esAcierto) {
-            $_SESSION['aciertos']++;
-            $_SESSION['encontradas'][] = $_SESSION['intento_actual'][0];
-            $_SESSION['encontradas'][] = $_SESSION['intento_actual'][1];
-            $_SESSION['intento_actual'] = array();
+            $juego['aciertos']++;
+            $juego['encontradas'][] = $juego['intento_actual'][0];
+            $juego['encontradas'][] = $juego['intento_actual'][1];
+            $juego['intento_actual'] = array();
         }
+        return $juego;
     }
 
     // Obtener la carta seleccionada
     if (isset($_GET['carta'])) {
-        $cartas = $_SESSION['cartas'];
-        registrarCarta($cartas);
+        $cartas = $juego['cartas'];
+        $juego = registrarCarta($juego);
+        assert ($juego);
     }
 
     if (isset($_GET['ocultar'])){
-        $_SESSION['intento_actual'] = array();
+        $juego['intento_actual'] = array();
 }
+
+$_SESSION['juego'] = $juego;
 ?>
 
 <!DOCTYPE html>
@@ -79,11 +93,13 @@
         <div>
             <div class="board">
                 <?php
-                    $cartas = $_SESSION['cartas'];
+
+
+                    $cartas = $juego['cartas'];
                     for ($i = 0; $i < count($cartas); $i++) {
-                        if (in_array($i, $_SESSION['intento_actual']) ) {
+                        if (in_array($i, $juego['intento_actual']) ) {
                             echo '<div class="carta descubierta">'.$cartas[$i].'</div>';
-                        } elseif (in_array($i, $_SESSION['encontradas'])) {
+                        } elseif (in_array($i, $juego['encontradas'])) {
                             echo '<div class="carta encontrada">'.$cartas[$i].'</div>';
                         } else {
                             echo '<a href="?carta='.$i.'"><div class="carta"></div></a>';
@@ -95,20 +111,17 @@
 
         <div class="info">
             <div>
-                <p>Intentos: <?php echo $_SESSION['intentos']; ?></p>
-                <p>Aciertos: <?php echo $_SESSION['aciertos']; ?></p>
+                <p>Intentos: <?php echo $juego['intentos']; ?></p>
+                <p>Aciertos: <?php echo $juego['aciertos']; ?></p>
                 <?php
-                    if ($debug){
-                        echo "<p>intento_actual: ". arrayToString($_SESSION['intento_actual'])."</p>";
-                        echo "<p>encontradas: ". arrayToString($_SESSION['encontradas'])."</p>";
-                    }
-                    if (count($_SESSION['intento_actual'])==2){
+
+                    if (count($juego['intento_actual'])==2){
                         // Agregar el enlace para ocultar las cartas
                         echo '<p><button onclick="location.href=\'?ocultar=true\'">Ocultar cartas</button></p>';
                     }
                     // Verificar si el juego ha terminado
-                    if ($_SESSION['aciertos'] == count($_SESSION['cartas'])/2) {
-                        echo "<p>¡Felicidades! Has ganado el juego en ".$_SESSION['intentos']." intentos.</p>";
+                    if ($juego['aciertos'] == count($juego['cartas'])/2) {
+                        echo "<p>¡Felicidades! Has ganado el juego en ".$juego['intentos']." intentos.</p>";
                         echo '<button onclick="location.href=\'index.php?restart=true\'" type=button>Jugar de nuevo</button>';
                     }
                 ?>
