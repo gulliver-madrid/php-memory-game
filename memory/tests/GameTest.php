77304582dc977e3908d0fc8a1e53c61d9b0b9901
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2023-07-19 19:55:22 +0200

    Create first test

---
+++ memory/tests/GameTest.php
@@ -0,0 +1,30 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+    require_once "memory/game.php";
+
+    function crearValoresCartasEjemplo(): array {
+        return ['a', 'b', 'c'];
+    }
+
+    class GameTest extends TestCase
+    {
+        public function testGameOnePlayer()
+        {
+            $juego = new Juego(crearValoresCartasEjemplo(), 1);
+            $this->assertJuegoBienInicializado($juego);
+            $this->assertEquals(1, count($juego->aciertos));
+        }
+        public function testGameTwoPlayers()
+        {
+            $juego = new Juego(crearValoresCartasEjemplo(), 2);
+            $this->assertJuegoBienInicializado($juego);
+            $this->assertEquals(2, count($juego->aciertos));
+        }
+        private function assertJuegoBienInicializado (Juego $juego)
+        {
+            $this->assertEquals(0, count($juego->encontradas));
+            $this->assertEquals(0, count($juego->encontradas_este_turno));
+            $this->assertEquals(1, ($juego->turno));
+        }
+    }
+?>
